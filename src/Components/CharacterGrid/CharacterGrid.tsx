d9b568968f9cdea2b19e531b0bc85b00702114f2
VDA <Vin-dellavv@github.com>
2023-10-02 17:22:49 +0200

    ADD/CH added virtual scroll on charList small view

--- src/Components/CharacterGrid/CharacterGrid.tsx
+++ src/Components/CharacterGrid/CharacterGrid.tsx
@@ -1,39 +1,47 @@
 import React from "react";
 import { JSX } from "react";
 
-import { List } from "antd";
+import { Grid, AutoSizer } from "react-virtualized";
 import CharacterCard from "../CharacterCard/CharacterCard";
 import type { CharacterLs } from "../../Models/queries.model";
 import "./CharacterGrid.css";
-
-const { Item } = List;
 
 const CharacterGrid = ({
 	characterLs
 }: {
 	characterLs: CharacterLs;
 }): JSX.Element => {
+	const renderCell = ({ columnIndex, rowIndex, key, style }: any) => {
+		const index = rowIndex * 3 + columnIndex;
+		if (index < characterLs.length) {
+			return (
+				<div key={key} style={style}>
+					<CharacterCard charData={characterLs[index]} />
+				</div>
+			);
+		}
+		return null;
+	};
+
 	return (
 		<>
 			<h2>Characters list</h2>
-			<List // TODO add virtual list when antd implement it
-				grid={{
-					gutter: 16,
-					xs: 1,
-					sm: 1,
-					md: 2,
-					lg: 3,
-					xl: 4,
-					xxl: 6
-				}}
-				dataSource={characterLs}
-				renderItem={item => (
-					<Item>
-						<CharacterCard charData={item} />
-					</Item>
-				)}
-				className="charList"
-			/>
+			<div className="char__grid">
+				{/* FIXME awful responsive handling */}
+				<AutoSizer>
+					{({ height, width }) => (
+						<Grid
+							height={height}
+							width={width}
+							rowCount={Math.ceil(characterLs.length / 2)}
+							columnCount={3}
+							rowHeight={450}
+							columnWidth={width / 3}
+							cellRenderer={renderCell}
+						/>
+					)}
+				</AutoSizer>
+			</div>
 		</>
 	);
 };
